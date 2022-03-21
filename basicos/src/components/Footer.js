ceaf8dc7d80f16d0481d50500782963a88dca91b
Jordi Cruz <jordi.cruz@ordermygear.com>
2022-03-21 13:36:45 -0600

    Update course

--- basicos/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Footer = ({fecha}) => (  
-        <footer>
-            <p>Todos los derechos reservados {fecha}</p>
-        </footer>
-    );
- 
-export default Footer;
--- clima/src/components/Error.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Error = ({mensaje}) => {
-    return (
-        <p className="red darken-4 error">{mensaje}</p>
-    );
-}
- 
-export default Error;
